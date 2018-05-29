39efe38fc565753ec001f6486c2374531b8a4332
WEBARTIST\User <hennasingh04@gmail.com>
2018-05-29 13:23:55 +0100

    Initial commit  added FabDemo and ScrollEventDemo

---
+++ app/src/main/java/com/artist/web/materialdesigndemos/FabDemo.java
@@ -0,0 +1,13 @@
+package com.artist.web.materialdesigndemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FabDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_demo);
+    }
+}
---
+++ app/src/main/java/com/artist/web/materialdesigndemos/ScrollEventDemo.java
@@ -0,0 +1,13 @@
+package com.artist.web.materialdesigndemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollEventDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_event);
+    }
+}
